b86c7fb794744288cf92e72581763a09a5f34d76
Heejeong01110 <123rkdrkd@naver.com>
2025-04-07 01:55:07 +0900

    chore: initial project setup

---
+++ src/main/java/com/heez/urlib/UrlibApplication.java
@@ -0,0 +1,13 @@
+package com.heez.urlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlibApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UrlibApplication.class, args);
+  }
+
+}
