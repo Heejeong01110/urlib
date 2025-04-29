c2945dfe6ce11983168e9de9a66fdd43ada5c0da
Heejeong01110 <123rkdrkd@naver.com>
2025-04-30 05:01:31 +0900

    feat: global exception handler 및 error message 추가

--- src/main/java/com/heez/urlib/global/exception/ErrorMessage.java
+++ src/main/java/com/heez/urlib/global/exception/ErrorMessage.java
@@ -4,6 +4,7 @@ package com.heez.urlib.global.exception;
 public enum ErrorMessage {
 
   USER_NOT_FOUND("{0}에 해당하는 사용자 정보가 없습니다."),
+  OAUTH_USER_NOT_FOUND("{0}에 해당하는 소셜 사용자 정보가 없습니다."),
   EMAIL_DUPLICATED("{0}은 중복되는 이메일 입니다."),
   NICKNAME_DUPLICATED("{0}은 중복되는 닉네임 입니다."),
   PASSWORD_VALID_FAIL("비밀번호가 잘못되었습니다."),
