c8da661e1e9bf2a35e4d48ce78b24a30a0f9be38
Heejeong01110 <123rkdrkd@naver.com>
2025-05-26 17:12:43 +0900

    rename: LinkCreateRequest 클래스명 변경 BaseLinkRequest - 사용성 확장

--- src/main/java/com/heez/urlib/domain/link/controller/dto/BaseLinkRequest.java
+++ src/main/java/com/heez/urlib/domain/link/controller/dto/BaseLinkRequest.java
@@ -1,10 +1,13 @@
 package com.heez.urlib.domain.link.controller.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
-public record LinkCreateRequest(
+public record BaseLinkRequest(
+    @NotBlank
     String title,
+    @NotBlank
     String url
 ) {
 
