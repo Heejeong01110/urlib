1ae3e83c6e75242688dc949c54974b9abfd8cec2
Heejeong01110 <123rkdrkd@naver.com>
2025-05-26 17:15:23 +0900

    feat: BookmarkCreateRequest valid 어노테이션 추가

--- src/main/java/com/heez/urlib/domain/bookmark/controller/dto/BookmarkCreateRequest.java
+++ src/main/java/com/heez/urlib/domain/bookmark/controller/dto/BookmarkCreateRequest.java
@@ -1,17 +1,24 @@
 package com.heez.urlib.domain.bookmark.controller.dto;
 
-import com.heez.urlib.domain.link.controller.dto.LinkCreateRequest;
+import com.heez.urlib.domain.link.controller.dto.BaseLinkRequest;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 import lombok.Builder;
 
 @Builder
 public record BookmarkCreateRequest(
-    String imageUrl,
+
+    @NotBlank(message = "제목을 입력해주세요.")
+    @Size(max = 100, message = "제목은 최대 100자까지 입력 가능합니다.")
     String title,
-    List<String> tags,
+
+    @Size(max = 500, message = "설명은 최대 500자까지 입력 가능합니다.")
     String description,
+    String imageUrl,
     Boolean visibleToOthers,
-    List<LinkCreateRequest> links
+    List<String> tags,
+    List<BaseLinkRequest> links
 ) {
 
 }
