8acba64fa195b04dd63661da977bd827ff724ab0
Heejeong01110 <123rkdrkd@naver.com>
2025-06-02 01:27:49 +0900

    feat: MemberSummaryResponse dto ststic 생성자 추가

--- src/main/java/com/heez/urlib/domain/member/controller/dto/MemberSummaryResponse.java
+++ src/main/java/com/heez/urlib/domain/member/controller/dto/MemberSummaryResponse.java
@@ -1,8 +1,17 @@
 package com.heez.urlib.domain.member.controller.dto;
 
+import com.heez.urlib.domain.member.service.dto.MemberSummaryProjection;
+
 public record MemberSummaryResponse(
     Long memberId,
     String memberImageUrl
 ) {
+
+  public static MemberSummaryResponse from(MemberSummaryProjection projection) {
+    return new MemberSummaryResponse(
+        projection.getId(),
+        projection.getImageUrl()
+    );
+  }
 
 }
