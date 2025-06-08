45b6b7b251a47238a68cdbcede2458da876017fa
Heejeong01110 <123rkdrkd@naver.com>
2025-06-08 18:46:33 +0900

    fix: AbstractGlobalException -> AbstractJwtException 상속으로 변경

--- src/main/java/com/heez/urlib/domain/auth/exception/ExpiredRefreshTokenException.java
+++ src/main/java/com/heez/urlib/domain/auth/exception/ExpiredRefreshTokenException.java
@@ -1,9 +1,9 @@
 package com.heez.urlib.domain.auth.exception;
 
-import com.heez.urlib.global.error.exception.AbstractGlobalException;
+import com.heez.urlib.global.error.exception.AbstractJwtException;
 import com.heez.urlib.global.error.handler.ErrorCode;
 
-public class ExpiredRefreshTokenException extends AbstractGlobalException {
+public class ExpiredRefreshTokenException extends AbstractJwtException {
 
   public ExpiredRefreshTokenException() {
     super(ErrorCode.EXPIRED_REFRESH_TOKEN);
--- src/main/java/com/heez/urlib/domain/auth/exception/InvalidRefreshTokenException.java
+++ src/main/java/com/heez/urlib/domain/auth/exception/InvalidRefreshTokenException.java
@@ -1,9 +1,9 @@
 package com.heez.urlib.domain.auth.exception;
 
-import com.heez.urlib.global.error.exception.AbstractGlobalException;
+import com.heez.urlib.global.error.exception.AbstractJwtException;
 import com.heez.urlib.global.error.handler.ErrorCode;
 
-public class InvalidRefreshTokenException extends AbstractGlobalException {
+public class InvalidRefreshTokenException extends AbstractJwtException {
 
   public InvalidRefreshTokenException() {
     super(ErrorCode.UNAUTHORIZED_TOKEN);
