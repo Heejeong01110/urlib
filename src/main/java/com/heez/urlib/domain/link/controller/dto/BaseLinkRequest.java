667b3555cb2c0cc925ae3b074ef7be1bab407c92
Heejeong01110 <123rkdrkd@naver.com>
2025-06-23 18:11:33 +0900

    feat: 링크 dto swagger annotation 추가

--- src/main/java/com/heez/urlib/domain/link/controller/dto/BaseLinkRequest.java
+++ src/main/java/com/heez/urlib/domain/link/controller/dto/BaseLinkRequest.java
@@ -1,15 +1,24 @@
 package com.heez.urlib.domain.link.controller.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
+
+@Schema(description = "링크 Base 요청 DTO")
 @Builder
 public record BaseLinkRequest(
+    @Schema(description = "링크 ID (수정 시 사용)", example = "101", nullable = true)
     Long id,
+
+    @Schema(description = "링크 제목", example = "google 홈페이지")
     @NotBlank
     String title,
+
+    @Schema(description = "링크 URL", example = "https://google.com")
     @NotBlank
     String url
+
 ) {
 
 }
--- src/main/java/com/heez/urlib/domain/link/controller/dto/LinkDetailResponse.java
+++ src/main/java/com/heez/urlib/domain/link/controller/dto/LinkDetailResponse.java
@@ -1,13 +1,21 @@
 package com.heez.urlib.domain.link.controller.dto;
 
 import com.heez.urlib.domain.link.model.Link;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
+@Schema(description = "링크 상세정보 응답 DTO")
 @Builder
 public record LinkDetailResponse(
+    @Schema(description = "링크 ID", example = "101")
     Long id,
+
+    @Schema(description = "링크 제목", example = "google 홈페이지")
     String title,
+
+    @Schema(description = "링크 URL", example = "https://google.com")
     String url
+
 ) {
 
   public static LinkDetailResponse from(Link link) {
