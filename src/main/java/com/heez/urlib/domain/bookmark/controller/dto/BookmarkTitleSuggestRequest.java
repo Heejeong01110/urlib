966c309033a7ca7a51c42cd445e2d908cec90a1f
Heejeong01110 <123rkdrkd@naver.com>
2025-07-17 18:12:47 +0900

    refactor: BookmarkTitlesSuggestRequest 변수 검증 추가

--- src/main/java/com/heez/urlib/domain/bookmark/controller/dto/BookmarkTitleSuggestRequest.java
+++ src/main/java/com/heez/urlib/domain/bookmark/controller/dto/BookmarkTitleSuggestRequest.java
@@ -1,10 +1,12 @@
 package com.heez.urlib.domain.bookmark.controller.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 
 public record BookmarkTitleSuggestRequest(
     @Schema(description = "링크 타이틀 목록", example = "LINK_TITLE_LIST")
+    @NotEmpty(message = "제목 리스트는 비어 있을 수 없습니다.")
     List<String> titles
 ) {
 
